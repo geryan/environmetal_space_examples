737a4cf8c0992701efaf0e468375677312b3dea8
geryan <geryan@users.noreply.github.com>
2026-06-05 16:33:06 +1000

    get more time periods

--- examples.R
+++ examples.R
@@ -8,21 +8,44 @@ library(geodata)
 # download a single tile of 12 months of average minimum temperature in area
 # of 30x30 degrees around Nigeria (0-30 degrees lon and lat)
 
-tmin <- cmip6_tile(
+# for present time
+tmin_t1 <- worldclim_tile(
   lon = 3.3,
   lat = 6.5,
-  model = "CNRM-CM6-1",
-  ssp = "370",
-  time = "2021-2040",
-  var="tmin",
+  var = "tmin",
   res = 10,
   path = tempdir() # you might want to set this so you don't need to repeatedly download
 )
 
-plot(tmin)
+# for future time
+tmin_t2 <- cmip6_tile(
+  lon = 3.3,
+  lat = 6.5,
+  model = "CNRM-CM6-1",
+  ssp = "370",
+  time = "2021-2040", # for more times repeat for other possibilities, see ?cmip6_tile
+  var = "tmin",
+  res = 10,
+  path = tempdir() # you might want to set this so you don't need to repeatedly download
+)
+
+plot(tmin_t1)
+plot(tmin_t2)
+
+
 
 # same fore precipitation (rainfall)
-prec <- cmip6_tile(
+# for present time
+prec_t1 <- worldclim_tile(
+  lon = 3.3,
+  lat = 6.5,
+  var = "prec",
+  res = 10,
+  path = tempdir() # you might want to set this so you don't need to repeatedly download
+)
+
+# for future time
+prec_t2 <- cmip6_tile(
   lon = 3.3,
   lat = 6.5,
   model = "CNRM-CM6-1",
@@ -33,25 +56,22 @@ prec <- cmip6_tile(
   path = tempdir() # you might want to set this so you don't need to repeatedly download
 )
 
-plot(prec)
-
+plot(prec_t1)
+plot(prec_t2)
 
 # standardise the data so mean = 0 and sd = 1
-tmin_scaled <- scale(tmin)
-prec_scaled <- scale(prec)
-
+tmin_t1_scaled <- scale(tmin_t1)
 
 # take some sample locations
-
 n_samples <- 100
 set.seed(20260505)
 
 samples <- spatSample(
-  x = tmin_scaled[[1]],
+  x = tmin_t1_scaled[[1]],
   size = n_samples,
   as.points = TRUE
 )
 
 # plot samples
-plot(tmin[[1]])
+plot(tmin_t1_scaled[[1]])
 points(samples)
